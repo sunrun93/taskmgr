48334e79fc1625372bd8c1f453976b74ba2375e7
sunrun93 <wangjian9302@163.com>
2019-04-23 22:53:10 +0800

    add form group and validation

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{ FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      email: ['wang@163.com', Validators.compose([Validators.required, Validators.email, this.validate]) ],
+      password: ['', Validators.required]
+    })
   }
 
+  onSubmit({value, valid}, ev: Event){
+    ev.preventDefault();
+    console.log(value);
+    console.log(valid);
+  }
+
+  validate(c: FormControl): { [key: string]: any } {
+    if(!c.valid){
+      return null;
+    }
+    const pattern = /^wang+/;
+    if(pattern.test(c.value)){
+      return null;
+    }
+    return {
+      emailNotValid:'The email must start with wang!'
+    }
+  }
 }
