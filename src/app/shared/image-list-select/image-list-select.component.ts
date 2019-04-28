a5c0fc68b237ab4850a6430727791c83372533e6
sunrun93 <wangjian9302@163.com>
2019-04-28 22:46:52 +0800

    add image-list-component

--- src/app/shared/image-list-select/image-list-select.component.ts
+++ src/app/shared/image-list-select/image-list-select.component.ts
@@ -1,9 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { FormControl, NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
 
 @Component({
   selector: 'app-image-list-select',
   templateUrl: './image-list-select.component.html',
-  styleUrls: ['./image-list-select.component.scss']
+  styleUrls: ['./image-list-select.component.scss'],
+  providers:[
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(()=>{ImageListSelectComponent}),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(()=>{ImageListSelectComponent}),
+      multi: true
+    }
+  ]
 })
 export class ImageListSelectComponent implements OnInit {
   @Input() title = '选择';
@@ -14,7 +27,7 @@ export class ImageListSelectComponent implements OnInit {
   @Input() imageWidth = '80px';
 
   selected: string;
-
+  private propagateChange = (_: any) => {}
   constructor() { }
 
   ngOnInit() {
@@ -22,6 +35,25 @@ export class ImageListSelectComponent implements OnInit {
 
   change(i){
     this.selected = this.items[i];
+    this.propagateChange(this.selected);
+  }
+
+  writeValue(obj:any):void{
+    this.selected = obj;
+  }
+
+  registerOnChange(fn:any):void{
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn:any):void{}
+
+  validate(c: FormControl): { [key: string]: any } {
+    return this.selected ? null : {
+      imsgeListInvalid: {
+        valid: false
+      }
+    }
   }
 
 }
